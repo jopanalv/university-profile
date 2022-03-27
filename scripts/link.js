ae9315db471f8d4b28def4a45bdd1ded09c31668
jopanalv <jopan.alv@gmail.com>
2022-03-27 18:27:44 +0700

    fix: hyperlink berita

---
+++ scripts/link.js
@@ -0,0 +1,8 @@
+async function getDetailBerita() {
+    let main = document.querySelector("#body-content");
+
+    const response = await fetch('./pages/detail_berita.html');
+    const detailBerita = await response.text();
+
+    main.innerHTML = detailBerita;
+}
